36e4ed73b2cb3cdf43c8f23631aa15659e1610b7
Luis García <pruebasdaw2021@gmail.com>
2023-02-08 13:50:19 +0100

    Creada base de datos database_links

---
+++ database/db.sql
@@ -0,0 +1,25 @@
+DROP DATABASE IF EXISTS database_links;
+CREATE DATABASE database_links CHARSET utf8mb4;
+USE database_links;
+
+-- USERS TABLE
+CREATE TABLE users(
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(20) NOT NULL,
+    password VARCHAR(60) NOT NULL,
+    email VARCHAR(120) NOT NULL,
+    fullname VARCHAR(100) NOT NULL
+);
+
+
+-- LINKS TABLE
+
+CREATE TABLE links(
+    id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    url VARCHAR(255) NOT NULL,
+    description TEXT,
+    user_id INT UNSIGNED,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_user FOREIGN KEY(user_id) REFERENCES users(id)
+);
