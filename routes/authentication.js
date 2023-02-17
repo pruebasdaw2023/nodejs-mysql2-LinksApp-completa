ef94f425d3d679d50c053c4171d02539ae321877
Luis García <pruebasdaw2021@gmail.com>
2023-02-17 10:06:23 +0100

    Empezamos diseño index.hbs

--- routes/authentication.js
+++ routes/authentication.js
@@ -2,23 +2,24 @@ const express = require('express');
 const { authenticate } = require('passport');
 const router = express.Router();
 const passport = require('passport')
+const { isLoggedIn, isNotLogged }= require('../lib/auth')
 
 /* GET users listing. */
-router.get('/signup', (req, res, next) => {
+router.get('/signup',isNotLogged, (req, res, next) => {
   res.render('auth/signup');
 });
 
-router.post('/signup', passport.authenticate('local.signup', {
+router.post('/signup',isNotLogged, passport.authenticate('local.signup', {
   successRedirect: '/profile',
   failureRedirect: '/signup',
   failureFlash: true
 }))
 
-router.get('/signin', (req, res) => {
+router.get('/signin',isNotLogged, (req, res) => {
   res.render('auth/signin')
 })
 
-router.post('/signin', (req, res, next) => {
+router.post('/signin',isNotLogged, (req, res, next) => {
   passport.authenticate('local.signin', {
     successRedirect: '/profile',
     failureRedirect: '/signin',
@@ -26,11 +27,11 @@ router.post('/signin', (req, res, next) => {
   })(req, res, next)
 })
 
-router.get('/profile', (req, res) => {
+router.get('/profile', isLoggedIn, (req, res) => {
   res.render('profile')
 })
 
-router.get('/logout', (req, res) => {  
+router.get('/logout',isLoggedIn, (req, res) => {  
   req.logOut(function(err){
     if (err) return next(err)
   })  
--- routes/links.js
+++ routes/links.js
@@ -3,19 +3,21 @@ const router = express.Router();
 
 const pool = require('../database')
 
+const { isLoggedIn } = require('../lib/auth')
+
 /* GET users listing. */
-router.get('/', async (req, res, next) => {
+router.get('/',isLoggedIn, async (req, res, next) => {
   const [ links ] = await pool.query('SELECT * FROM links')
   console.log(links)
   res.render('links/list', { links })
   
 });
 
-router.get('/add', (req, res) => {
+router.get('/add',isLoggedIn, (req, res) => {
   res.render('links/add')
 })
 
-router.post('/add', async (req, res) => {
+router.post('/add',isLoggedIn, async (req, res) => {
 
   const { title, url, description } = req.body
   const newLink = {
@@ -29,21 +31,21 @@ router.post('/add', async (req, res) => {
 })
 
 
-router.get('/delete/:id', async (req, res) => {
+router.get('/delete/:id',isLoggedIn, async (req, res) => {
   const { id } = req.params
   await pool.query('DELETE FROM links WHERE id = ?', [id])
   req.flash('success', 'Link removed successfully')
   res.redirect('/links')
 }
 )
-router.get('/edit/:id', async (req, res) => {
+router.get('/edit/:id',isLoggedIn, async (req, res) => {
   const { id } = req.params
   const [ link ] = await pool.query('SELECT * FROM links WHERE id = ?', [id])
   
   res.render('links/edit', {link:link[0]})
 })
 
-router.post('/edit/:id', async (req, res) => {
+router.post('/edit/:id',isLoggedIn, async (req, res) => {
   const { id } = req.params
   const { title, url, description} = req.body
   const newLink = {
